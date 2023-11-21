a0c593fdcd8f747ef560c3e73609d005df851ced
sw.lee <swlee173@gmail.com>
2023-11-21 18:48:43 +0900

    한 건, 목록 select

--- src/main/java/jpamember/start/JpaMain.java
+++ src/main/java/jpamember/start/JpaMain.java
@@ -22,13 +22,23 @@ public class JpaMain {
     }
 
     public static void logic(EntityManager em) {
-        String id = "id1";
+        String id = "id3";
         Member member = new Member();
         member.setId(id);
         member.setUsername("LEE");
         member.setAge(2);
 
+        // 등록
         em.persist(member);
+        // 수정
         member.setAge(20);
+
+        // 한 건 조회
+        Member findMember = em.find(Member.class, id);
+        System.out.println("findMember : " + findMember.getUsername() + ", age : " + +findMember.getAge());
+
+        // 목록 조회
+        List<Member> members = em.createQuery("select m from Member m", Member.class).getResultList();
+        System.out.println("members : " + members.size());
     }
 }
